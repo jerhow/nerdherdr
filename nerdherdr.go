a8dbe1d3c07b2b72bc7a52b97f5cde388cd7b5a7
Jerry Howard <jerryhoward@fastmail.fm>
2017-12-21 20:36:21 +0000

    First POC using html/template to render a page with some dynamic bits. Builds/runs correctly.

--- nerdherdr.go
+++ nerdherdr.go
@@ -3,10 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
 
 func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "GET /movies (All Movies). Not implemented yet. PORT env var: "+os.Getenv("PORT"))
@@ -28,6 +39,19 @@ func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "DELETE /movies (Delete Movie). Not implemented yet.")
 }
 
+func RenderTmpl1(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("tmpl1.html"))
+	data := TodoPageData{
+		PageTitle: "My TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
+}
+
 // Get the Port from the environment so we can run on Heroku
 func GetPort() string {
 	var port = os.Getenv("PORT") // There's no way to know this ahead of time
@@ -46,6 +70,7 @@ func main() {
 	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
 	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
 	r.HandleFunc("/movies/{id}", FindMovieEndPoint).Methods("GET")
+	r.HandleFunc("/tmpl1", RenderTmpl1).Methods("GET")
 
 	if err := http.ListenAndServe(GetPort(), r); err != nil {
 		log.Fatal(err)
