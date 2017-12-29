0a9c9da762b817f6631336dc1281d304159c451f
Jerry Howard <jerryhoward@fastmail.fm>
2017-12-29 03:34:05 +0000

    Creates a package 'route', for route handlers. Also fixes a bug with the index page.

---
+++ internal/routes/routes.go
@@ -0,0 +1,141 @@
+// Package routes is the place for route (endpoint) handlers.
+// These functions are named in the route declarations for the mux router.
+package routes
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+func AllMovies(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "GET /movies (All Movies). Not implemented yet. PORT env var: "+os.Getenv("PORT"))
+}
+
+func FindMovie(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	fmt.Fprintf(w, "ID: %s", id)
+	// fmt.Fprintln(w, "GET /movies/{id} (Find Movie). Not implemented yet, fool!")
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	// fmt.Printf("%+v\n", r)
+	id := r.FormValue("id")
+	name := r.PostFormValue("name")
+	fmt.Fprintf(w, "Hello, %s! ID: %s", name, id)
+	// fmt.Fprintln(w, "POST /movies (Create Movie). Not implemented yet.")
+}
+
+func UpdateMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "PUT /movies (Update Movie). Not implemented yet.")
+}
+
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "DELETE /movies (Delete Movie). Not implemented yet.")
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	type PageData struct {
+		PageTitle string
+		BodyTitle string
+		LoginMsg  string
+	}
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	data := PageData{
+		PageTitle: "Nerdherdr: Tools for Technical Managers",
+		BodyTitle: "Welcome!",
+		LoginMsg:  "",
+	}
+	tmpl.Execute(w, data)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	type PageData struct {
+		PageTitle string
+		BodyTitle string
+		LoginMsg  string
+	}
+
+	var un, pw string
+	data := PageData{
+		PageTitle: "Nerdherdr: Tools for Technical Managers",
+		BodyTitle: "Welcome!",
+	}
+
+	un = r.PostFormValue("un")
+	pw = r.PostFormValue("pw")
+
+	if un == "" || pw == "" {
+		// fmt.Println("un is an empty string, no value provided")
+		data.LoginMsg = "Invalid login"
+	} else {
+		// fmt.Printf("%+v\n", un)
+		data.LoginMsg = "Valid login :)"
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, data)
+
+	// driver := "mysql"
+	// dsn := dbDsn()
+	// db, err := sql.Open(driver, dsn)
+	// util.ErrChk(err)
+	// defer db.Close()
+
+	// err = db.Ping()
+	// util.ErrChk(err)
+
+	// err = db.QueryRow(
+	// 	"SELECT l_name, f_initial FROM t_users WHERE id = ?", 1).Scan(&LName, &FInitial)
+
+	// switch {
+	// case err == sql.ErrNoRows:
+	// 	fmt.Println("No user with that ID")
+	// case err != nil:
+	// 	log.Fatal(err)
+	// default:
+	// 	fmt.Printf("\nUSER: %s, %s\n", LName, FInitial)
+	// }
+}
+
+// ===========================================================
+// This stuff below was from a tutorial. Will clean up soon.
+//
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	BodyTitle string
+	Todos     []Todo
+}
+
+func Tmpl1(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/tmpl1.html"))
+	data := TodoPageData{
+		PageTitle: "Template 1",
+		BodyTitle: "My TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
+}
+
+func Tmpl2(w http.ResponseWriter, r *http.Request) {
+	type PageData struct {
+		PageTitle string
+		BodyTitle string
+	}
+	tmpl := template.Must(template.ParseFiles("templates/tmpl2.html"))
+	data := PageData{
+		PageTitle: "Template 2",
+		BodyTitle: "This is the second template",
+	}
+	tmpl.Execute(w, data)
+}
--- nerdherdr.go
+++ nerdherdr.go
@@ -4,77 +4,13 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/jerhow/nerdherdr/internal/db"
+	"github.com/jerhow/nerdherdr/internal/routes"
 	"github.com/jerhow/nerdherdr/internal/util"
 	"golang.org/x/crypto/bcrypt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
-
-type Todo struct {
-	Title string
-	Done  bool
-}
-
-type TodoPageData struct {
-	PageTitle string
-	BodyTitle string
-	Todos     []Todo
-}
-
-func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "GET /movies (All Movies). Not implemented yet. PORT env var: "+os.Getenv("PORT"))
-}
-
-func FindMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	id := r.FormValue("id")
-	fmt.Fprintf(w, "ID: %s", id)
-	// fmt.Fprintln(w, "GET /movies/{id} (Find Movie). Not implemented yet, fool!")
-}
-
-func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	// fmt.Printf("%+v\n", r)
-	id := r.FormValue("id")
-	name := r.PostFormValue("name")
-	fmt.Fprintf(w, "Hello, %s! ID: %s", name, id)
-	// fmt.Fprintln(w, "POST /movies (Create Movie). Not implemented yet.")
-}
-
-func UpdateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "PUT /movies (Update Movie). Not implemented yet.")
-}
-
-func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "DELETE /movies (Delete Movie). Not implemented yet.")
-}
-
-func RenderTmpl1(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/tmpl1.html"))
-	data := TodoPageData{
-		PageTitle: "Template 1",
-		BodyTitle: "My TODO list",
-		Todos: []Todo{
-			{Title: "Task 1", Done: false},
-			{Title: "Task 2", Done: true},
-			{Title: "Task 3", Done: true},
-		},
-	}
-	tmpl.Execute(w, data)
-}
-
-func RenderTmpl2(w http.ResponseWriter, r *http.Request) {
-	type PageData struct {
-		PageTitle string
-		BodyTitle string
-	}
-	tmpl := template.Must(template.ParseFiles("templates/tmpl2.html"))
-	data := PageData{
-		PageTitle: "Template 2",
-		BodyTitle: "This is the second template",
-	}
-	tmpl.Execute(w, data)
-}
 
 // Get the Port from the environment so we can run on Heroku
 func GetPort() string {
@@ -85,68 +21,6 @@ func GetPort() string {
 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
 	}
 	return ":" + port
-}
-
-func IndexEndPoint(w http.ResponseWriter, r *http.Request) {
-	type PageData struct {
-		PageTitle string
-		BodyTitle string
-	}
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	data := PageData{
-		PageTitle: "Nerdherdr: Tools for Technical Managers",
-		BodyTitle: "Welcome!",
-	}
-	tmpl.Execute(w, data)
-}
-
-func LoginEndPoint(w http.ResponseWriter, r *http.Request) {
-	type PageData struct {
-		PageTitle string
-		BodyTitle string
-		LoginMsg  string
-	}
-
-	var un, pw string
-	data := PageData{
-		PageTitle: "Nerdherdr: Tools for Technical Managers",
-		BodyTitle: "Welcome!",
-	}
-
-	un = r.PostFormValue("un")
-	pw = r.PostFormValue("pw")
-
-	if un == "" || pw == "" {
-		// fmt.Println("un is an empty string, no value provided")
-		data.LoginMsg = "Invalid login"
-	} else {
-		// fmt.Printf("%+v\n", un)
-		data.LoginMsg = "Valid login :)"
-	}
-
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl.Execute(w, data)
-
-	// driver := "mysql"
-	// dsn := dbDsn()
-	// db, err := sql.Open(driver, dsn)
-	// util.ErrChk(err)
-	// defer db.Close()
-
-	// err = db.Ping()
-	// util.ErrChk(err)
-
-	// err = db.QueryRow(
-	// 	"SELECT l_name, f_initial FROM t_users WHERE id = ?", 1).Scan(&LName, &FInitial)
-
-	// switch {
-	// case err == sql.ErrNoRows:
-	// 	fmt.Println("No user with that ID")
-	// case err != nil:
-	// 	log.Fatal(err)
-	// default:
-	// 	fmt.Printf("\nUSER: %s, %s\n", LName, FInitial)
-	// }
 }
 
 func pepper() string {
@@ -167,17 +41,16 @@ func checkPasswordHash(pwd string, hash string) bool {
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", IndexEndPoint).Methods("GET")
-	r.HandleFunc("/login", LoginEndPoint).Methods("POST")
-	r.HandleFunc("/movies", AllMoviesEndPoint).Methods("GET")
-	r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
-	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
-	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
-	r.HandleFunc("/movies/{id}", FindMovieEndPoint).Methods("GET")
-	r.HandleFunc("/tmpl1", RenderTmpl1).Methods("GET")
-	r.HandleFunc("/tmpl2", RenderTmpl2).Methods("GET")
+	r.HandleFunc("/", routes.Index).Methods("GET")
+	r.HandleFunc("/login", routes.Login).Methods("POST")
+	r.HandleFunc("/movies", routes.AllMovies).Methods("GET")
+	r.HandleFunc("/movies", routes.CreateMovie).Methods("POST")
+	r.HandleFunc("/movies", routes.UpdateMovie).Methods("PUT")
+	r.HandleFunc("/movies", routes.DeleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", routes.FindMovie).Methods("GET")
+	r.HandleFunc("/tmpl1", routes.Tmpl1).Methods("GET")
+	r.HandleFunc("/tmpl2", routes.Tmpl2).Methods("GET")
 
-	// fmt.Println(util.Hi("Jerry"))
 	fmt.Println(db.Doit("Jerry"))
 	fmt.Println(util.AddTwoInts(1, 2))
 	db.Db1()
