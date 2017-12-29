8afa6167b38812c7775f34dcd4b8ec8bdd6f2321
Jerry Howard <jerryhoward@fastmail.fm>
2017-12-29 02:54:23 +0000

    Establishes an internal 'db' library, and relocates all appropriate functions into it. Also takes in the vendor'd assets for bcrypt, since I forgot to rebuild my deps earlier.

---
+++ internal/db/db.go
@@ -0,0 +1,135 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql" // Imports the package solely for its side-effects
+	"github.com/jerhow/nerdherdr/internal/util"
+	"log"
+	"strconv"
+)
+
+func Doit(s string) string {
+	return util.Hi(s)
+}
+
+func dsn() string {
+	dbUser := "jerry"
+	dbPass := "pass"
+	dbHost := "go_mysql_1"
+	dbPort := "3306"
+	dbName := "nerdherdr01"
+
+	connStr := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName
+	return connStr
+}
+
+func Db1() {
+	fmt.Println("sup from db()")
+	driver := "mysql"
+	dsn := dsn()
+	dbh, err := sql.Open(driver, dsn)
+	util.ErrChk(err)
+	defer dbh.Close()
+
+	err = dbh.Ping()
+	util.ErrChk(err)
+
+	stmtIns, err := dbh.Prepare("INSERT INTO t_users (l_name, f_initial) VALUES (?, ?)")
+	util.ErrChk(err)
+	defer stmtIns.Close()
+
+	_, err2 := stmtIns.Exec("Franklin", "A")
+	util.ErrChk(err2)
+
+	// insert, err := db.Query("INSERT INTO t_users (l_name, f_initial) VALUES ('Franklin', 'A')")
+	// if err != nil {
+	// 	log.Fatal(err) // panic(err.Error)
+	// }
+	// defer insert.Close()
+}
+
+func DbPopulateStruct() {
+	fmt.Println("sup from dbPopulateStruct()")
+
+	// Tag... - a very simple struct
+	type Tag struct {
+		Id       int    `json:"id"`
+		Lname    string `json:"l_name"`
+		Finitial string `json:"f_initial"`
+	}
+
+	driver := "mysql"
+	dsn := dsn()
+	dbh, err := sql.Open(driver, dsn)
+	util.ErrChk(err)
+	defer dbh.Close()
+
+	err = dbh.Ping()
+	util.ErrChk(err)
+
+	// stmt, err := db.Prepare("SELECT id, l_name, f_initial FROM t_users WHERE id > ?")
+	// util.ErrChk(err)
+	// defer stmt.Close()
+	//
+	// rows, err := stmt.Query(6)
+	// util.ErrChk(err)
+	// defer rows.Close()
+	//
+	// Regarding what's above ^^
+	// According to this:
+	// http://go-database-sql.org/prepared.html and http://go-database-sql.org/retrieving.html
+	// """"
+	// Go creates prepared statements for you under the covers.
+	// A simple db.Query(sql, param1, param2), for example, works by preparing the sql,
+	// then executing it with the parameters and finally closing the statement.
+	// """"
+	// Which means, the more verbose way we're doing it above is good when you want to
+	// explicitly manage the prepared statements you're spawning (as in for heavy reuse, or efficiency),
+	// but in other cases, the more concise method below is fine, since it does it all for you.
+	// See this as well for more details and concerns over efficiency where it concerns
+	// prepared statements. Basically, the preparation, execution and closing of the prepared
+	// statement constitute three separate round trips to the database (!).
+	// https://www.vividcortex.com/blog/2014/11/19/analyzing-prepared-statement-performance-with-vividcortex/
+	//
+	//
+	// THIS IS PERFECTLY FINE AS WELL:
+	rows, err := dbh.Query("SELECT id, l_name, f_initial FROM t_users WHERE id > ?", 6)
+	util.ErrChk(err)
+	defer rows.Close()
+
+	fmt.Println()
+	for rows.Next() { // for each row, scan the result into the tag object (struct)
+		var tag Tag
+		err := rows.Scan(&tag.Id, &tag.Lname, &tag.Finitial)
+		util.ErrChk(err)
+		fmt.Println(strconv.Itoa(tag.Id) + ": " + tag.Lname + ", " + tag.Finitial)
+	}
+	fmt.Println()
+}
+
+func DbSingleRowQuery() {
+	fmt.Println("sup from dbSingleRowQuery()")
+	var LName, FInitial string
+
+	driver := "mysql"
+	dsn := dsn()
+	dbh, err := sql.Open(driver, dsn)
+	util.ErrChk(err)
+	defer dbh.Close()
+
+	err = dbh.Ping()
+	util.ErrChk(err)
+
+	err = dbh.QueryRow(
+		"SELECT l_name, f_initial FROM t_users WHERE id = ?", 1).Scan(&LName, &FInitial)
+
+	switch {
+	case err == sql.ErrNoRows:
+		fmt.Println("No user with that ID")
+	case err != nil:
+		log.Fatal(err)
+	default:
+		fmt.Printf("\nUSER: %s, %s\n", LName, FInitial)
+	}
+}
--- nerdherdr.go
+++ nerdherdr.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"./internal/util" // "github.com/jerhow/nerdherdr/internal/util"
-	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql" // Imports the package solely for its side-effects
 	"github.com/gorilla/mux"
+	"github.com/jerhow/nerdherdr/internal/db"
+	"github.com/jerhow/nerdherdr/internal/util"
 	"golang.org/x/crypto/bcrypt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 )
 
 type Todo struct {
@@ -87,127 +85,6 @@ func GetPort() string {
 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
 	}
 	return ":" + port
-}
-
-func dbDsn() string {
-	dbUser := "jerry"
-	dbPass := "pass"
-	dbHost := "go_mysql_1"
-	dbPort := "3306"
-	dbName := "nerdherdr01"
-
-	connStr := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName
-	return connStr
-}
-
-func db() {
-	fmt.Println("sup from db()")
-	driver := "mysql"
-	dsn := dbDsn()
-	db, err := sql.Open(driver, dsn)
-	util.ErrChk(err)
-	defer db.Close()
-
-	err = db.Ping()
-	util.ErrChk(err)
-
-	stmtIns, err := db.Prepare("INSERT INTO t_users (l_name, f_initial) VALUES (?, ?)")
-	util.ErrChk(err)
-	defer stmtIns.Close()
-
-	_, err2 := stmtIns.Exec("Franklin", "A")
-	util.ErrChk(err2)
-
-	// insert, err := db.Query("INSERT INTO t_users (l_name, f_initial) VALUES ('Franklin', 'A')")
-	// if err != nil {
-	// 	log.Fatal(err) // panic(err.Error)
-	// }
-	// defer insert.Close()
-}
-
-func dbPopulateStruct() {
-	fmt.Println("sup from dbPopulateStruct()")
-
-	// Tag... - a very simple struct
-	type Tag struct {
-		Id       int    `json:"id"`
-		Lname    string `json:"l_name"`
-		Finitial string `json:"f_initial"`
-	}
-
-	driver := "mysql"
-	dsn := dbDsn()
-	db, err := sql.Open(driver, dsn)
-	util.ErrChk(err)
-	defer db.Close()
-
-	err = db.Ping()
-	util.ErrChk(err)
-
-	// stmt, err := db.Prepare("SELECT id, l_name, f_initial FROM t_users WHERE id > ?")
-	// util.ErrChk(err)
-	// defer stmt.Close()
-	//
-	// rows, err := stmt.Query(6)
-	// util.ErrChk(err)
-	// defer rows.Close()
-	//
-	// Regarding what's above ^^
-	// According to this:
-	// http://go-database-sql.org/prepared.html and http://go-database-sql.org/retrieving.html
-	// """"
-	// Go creates prepared statements for you under the covers.
-	// A simple db.Query(sql, param1, param2), for example, works by preparing the sql,
-	// then executing it with the parameters and finally closing the statement.
-	// """"
-	// Which means, the more verbose way we're doing it above is good when you want to
-	// explicitly manage the prepared statements you're spawning (as in for heavy reuse, or efficiency),
-	// but in other cases, the more concise method below is fine, since it does it all for you.
-	// See this as well for more details and concerns over efficiency where it concerns
-	// prepared statements. Basically, the preparation, execution and closing of the prepared
-	// statement constitute three separate round trips to the database (!).
-	// https://www.vividcortex.com/blog/2014/11/19/analyzing-prepared-statement-performance-with-vividcortex/
-	//
-	//
-	// THIS IS PERFECTLY FINE AS WELL:
-	rows, err := db.Query("SELECT id, l_name, f_initial FROM t_users WHERE id > ?", 6)
-	util.ErrChk(err)
-	defer rows.Close()
-
-	fmt.Println()
-	for rows.Next() { // for each row, scan the result into the tag object (struct)
-		var tag Tag
-		err := rows.Scan(&tag.Id, &tag.Lname, &tag.Finitial)
-		util.ErrChk(err)
-		fmt.Println(strconv.Itoa(tag.Id) + ": " + tag.Lname + ", " + tag.Finitial)
-	}
-	fmt.Println()
-}
-
-func dbSingleRowQuery() {
-	fmt.Println("sup from dbSingleRowQuery()")
-	var LName, FInitial string
-
-	driver := "mysql"
-	dsn := dbDsn()
-	db, err := sql.Open(driver, dsn)
-	util.ErrChk(err)
-	defer db.Close()
-
-	err = db.Ping()
-	util.ErrChk(err)
-
-	err = db.QueryRow(
-		"SELECT l_name, f_initial FROM t_users WHERE id = ?", 1).Scan(&LName, &FInitial)
-
-	switch {
-	case err == sql.ErrNoRows:
-		fmt.Println("No user with that ID")
-	case err != nil:
-		log.Fatal(err)
-	default:
-		fmt.Printf("\nUSER: %s, %s\n", LName, FInitial)
-	}
 }
 
 func IndexEndPoint(w http.ResponseWriter, r *http.Request) {
@@ -300,11 +177,12 @@ func main() {
 	r.HandleFunc("/tmpl1", RenderTmpl1).Methods("GET")
 	r.HandleFunc("/tmpl2", RenderTmpl2).Methods("GET")
 
-	fmt.Println(util.Hi("Jerry"))
+	// fmt.Println(util.Hi("Jerry"))
+	fmt.Println(db.Doit("Jerry"))
 	fmt.Println(util.AddTwoInts(1, 2))
-	db()
-	dbPopulateStruct()
-	dbSingleRowQuery()
+	db.Db1()
+	db.DbPopulateStruct()
+	db.DbSingleRowQuery()
 
 	fmt.Println("=================")
 	// fmt.Println(hashIt("wut"))
