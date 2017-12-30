83715ec4aaaff2a39bc3330a931c4ba380483a2f
Jerry Howard <jerryhoward@fastmail.fm>
2017-12-30 22:08:37 +0000

    Demonstrates basic functionality of cookie-based web sessions using the gorilla/sessions package. Working off of this example: https://gowebexamples.com/sessions/. I temporarily disabled a bunch of existing app stuff for this, but it's just for the POC.

--- nerdherdr.go
+++ nerdherdr.go
@@ -3,14 +3,50 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"github.com/jerhow/nerdherdr/internal/controllers"
-	"github.com/jerhow/nerdherdr/internal/db"
-	"github.com/jerhow/nerdherdr/internal/login"
-	"github.com/jerhow/nerdherdr/internal/util"
+	// "github.com/jerhow/nerdherdr/internal/db"
+	// loginreal "github.com/jerhow/nerdherdr/internal/login"
+	// "github.com/jerhow/nerdherdr/internal/util"
 	"log"
 	"net/http"
 	"os"
 )
+
+// NOTE: Key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
+var key = []byte("super-secret-key")
+var store = sessions.NewCookieStore(key)
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Check if user is authenticated
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	fmt.Fprintln(w, "The cake is a lie!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Authentication goes here
+	// ...
+
+	// Set user
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Revoke user's authentication
+	session.Values["authenticated"] = false
+	session.Save(r, w)
+}
 
 // Get the Port from the environment so we can run on Heroku
 func GetPort() string {
@@ -26,7 +62,7 @@ func GetPort() string {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Index).Methods("GET")
-	r.HandleFunc("/login", controllers.Login).Methods("POST")
+	r.HandleFunc("/loginreal", controllers.Login).Methods("POST")
 	r.HandleFunc("/welcome", controllers.Welcome).Methods("GET")
 	r.HandleFunc("/movies", controllers.AllMovies).Methods("GET")
 	r.HandleFunc("/movies", controllers.CreateMovie).Methods("POST")
@@ -36,24 +72,28 @@ func main() {
 	r.HandleFunc("/tmpl1", controllers.Tmpl1).Methods("GET")
 	r.HandleFunc("/tmpl2", controllers.Tmpl2).Methods("GET")
 
-	fmt.Println(db.Doit("Jerry"))
-	fmt.Println(util.AddTwoInts(1, 2))
-	db.Db1()
-	db.DbPopulateStruct()
-	db.DbSingleRowQuery()
+	r.HandleFunc("/secret", secret)
+	r.HandleFunc("/login", login)
+	r.HandleFunc("/logout", logout)
 
-	fmt.Println("=================")
-	// fmt.Println(hashIt("wut"))
-	pwd := "pass"
-	hash, _ := login.HashPwd(pwd)
-	fmt.Println("Password:", pwd)
-	fmt.Println("Hash:    ", hash)
+	// fmt.Println(db.Doit("Jerry"))
+	// fmt.Println(util.AddTwoInts(1, 2))
+	// db.Db1()
+	// db.DbPopulateStruct()
+	// db.DbSingleRowQuery()
 
-	// db.WritePwd(hash)
+	// fmt.Println("=================")
+	// // fmt.Println(hashIt("wut"))
+	// pwd := "pass"
+	// hash, _ := loginreal.HashPwd(pwd)
+	// fmt.Println("Password:", pwd)
+	// fmt.Println("Hash:    ", hash)
 
-	match := login.CheckPasswordHash(pwd, hash)
-	fmt.Println("Match:   ", match)
-	fmt.Println("=================")
+	// // db.WritePwd(hash)
+
+	// match := loginreal.CheckPasswordHash(pwd, hash)
+	// fmt.Println("Match:   ", match)
+	// fmt.Println("=================")
 
 	if err := http.ListenAndServe(GetPort(), r); err != nil {
 		log.Fatal(err)
