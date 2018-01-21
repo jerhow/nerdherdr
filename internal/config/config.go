b74eda470875d63713d3d0db66efd756aaeb157e
Jerry Howard <jerryhoward@fastmail.fm>
2018-01-21 15:58:18 +0000

    Flips the branding back on

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,7 +5,7 @@
 // be here (those should live in the ENV config variables).
 package config
 
-const DISPLAY_BRANDING bool = false
+const DISPLAY_BRANDING bool = true
 
 // "Tools for Technical Managers"
 // "Guiding the Human Side of Technology"
